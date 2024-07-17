511cf96d5afe62514abee79ba9b8b8fea448530b
maria <maria@gmail.com>
2024-07-17 17:21:05 -0400

    back home2

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -266,7 +266,7 @@ function ProductList(props) {
             <div className="tag">
                <div className="luxury" onClick={props.toLanding}>
                <img src="https://cdn.pixabay.com/photo/2020/08/05/13/12/eco-5465432_1280.png" alt="" />
-               <a href="/" style={{textDecoration:'none'}}>
+               <a style={{textDecoration:'none'}}>
                         <div>
                     <h3 style={{color:'white'}}>Paradise Nursery</h3>
                     <i style={{color:'white'}}>Where Green Meets Serenity</i>
